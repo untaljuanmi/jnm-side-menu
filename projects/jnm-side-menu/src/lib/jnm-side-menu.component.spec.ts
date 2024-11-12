97c86f72f1f31cf73807f857ccf9a95e41355205
Juanmi González <juanmi.gonzalezm@proton.me>
2024-11-12 12:54:39 +0000

    feat: test sign-in and sign-out buttons

--- projects/jnm-side-menu/src/lib/jnm-side-menu.component.spec.ts
+++ projects/jnm-side-menu/src/lib/jnm-side-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JnmSideMenuComponent } from './jnm-side-menu.component';
+import { provideRouter } from '@angular/router';
 
 describe('JnmSideMenuComponent', () => {
   let component: JnmSideMenuComponent;
@@ -8,9 +9,9 @@ describe('JnmSideMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [JnmSideMenuComponent]
-    })
-    .compileComponents();
+      imports: [JnmSideMenuComponent],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(JnmSideMenuComponent);
     component = fixture.componentInstance;
@@ -19,5 +20,29 @@ describe('JnmSideMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onSignIn when sign in button is clicked', () => {
+    spyOn(component.onSignIn, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', false);
+    fixture.detectChanges();
+    const button: HTMLButtonElement = fixture.nativeElement.querySelector(
+      '[data-sign-in]',
+    ) as HTMLButtonElement;
+    expect(button).toBeTruthy();
+    button.click();
+    expect(component.onSignIn.emit).toHaveBeenCalled();
+  });
+
+  it('should call onSignOut when sign out button is clicked', () => {
+    spyOn(component.onSignOut, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', true);
+    fixture.detectChanges();
+    const button: HTMLButtonElement = fixture.nativeElement.querySelector(
+      '[data-sign-out]',
+    ) as HTMLButtonElement;
+    expect(button).toBeTruthy();
+    button.click();
+    expect(component.onSignOut.emit).toHaveBeenCalled();
   });
 });
